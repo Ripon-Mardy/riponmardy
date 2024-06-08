6140d7b51d6aed41ca93e8199aabe52889610a10
Ripon-Mardy <mardyripon10@gmail.com>
2024-06-08 23:33:22 +0600

    change

---
+++ app/blogs/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div className='flex items-center flex-col justify-center h-screen'>
+            <h1>just a moment</h1>
+        </div>
+    )
+}
+
+export default loading
--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,10 +1,55 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+// ==== import blogs images ====
+import blog1 from './../../public/images/blog/blog1.png'
+
+// ==== json data ====
+import blogsJson from './../../blogs.json'
 
 const page = () => {
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        setBlogs(blogsJson)
+    }, [])
+
+    const textVariants = {
+        initial: { opacity: 0.3, y: 30 },
+        animate: { opacity: 1, y: 0 }
+    }
+
+
     return (
-        <>
-            <h1>Hello </h1>
-        </>
+        <div className='pb-5'>
+            <div
+                initial='initial'
+                animate='animate'
+                variants={textVariants}
+                transition={{ duration: 0.5 }}
+                className='leading-none'
+            >
+                <h1 className='text-3xl font-semibold text-headingColor capitalize'>
+                    Blogs
+                </h1>
+                <span className='text-sm italic text-paragraphColor leading-none font-semibold'>
+                    my all Blogs
+                </span>
+            </div>
+            {/* ==== blog content === */}
+            <div>
+                {
+                    blogs.map((item, index) => (
+                        <div>
+                            {item.title}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
