40d1632b88d7c02d4edc50d6ad28624ec14f2d79
Ripon-Mardy <mardyripon10@gmail.com>
2024-07-03 12:06:07 +0600

    chagne

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Lora } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
+import Script from "next/script";
+
 
 const lora = Lora({
   subsets: ["latin"],
@@ -17,6 +19,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+
+      <head>
+
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J4E4Y2SR22"></Script>
+        <Script>
+         {
+          `
+           window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J4E4Y2SR22');
+          `
+         }
+        </Script>
+
+      </head>
+
+
       <body suppressHydrationWarning={true} className={lora.className}>
         <div className="md:flex ">
           <div className="md:basis-[20%] md:bg-gray-800 md:h-full md:fixed md:w-[20%] relative md:px-5">
