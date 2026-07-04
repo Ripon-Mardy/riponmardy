a9f80fe1ac48889b5093b7559e6f132c034c8b50
Ripon-Mardy <mardyripon10@gmail.com>
2026-07-05 00:05:10 +0600

    add experice

--- components/Banner.js
+++ components/Banner.js
@@ -21,26 +21,38 @@ const Banner = () => {
         {/* text  */}
         <div className="text-left mt-5">
           <div className="space-y-3">
-            <h2 className="text-3xl md:text-6xl font-semibold relative flex" >
-            Hi, I'm Ripon Mardy
-            <span className="text-sm bg-gray-100 h-fit px-2 rounded-md ml-2 font-thin border border-gray-200 hidden md:inline-block">
-              Software Engineer
-            </span>
-          </h2>
-          <h3 className="text-lg md:text-2xl">Full Stack Web Developer with 4+ years of experience.</h3>
-          <p className="text-lg md:text-2xl">I <strong>design</strong>, <strong>code</strong>, and <strong>deploy</strong> things that live on the web.</p>
+            <h2 className="text-3xl md:text-6xl font-semibold relative flex">
+              Hi, I'm Ripon Mardy
+              <span className="text-sm bg-gray-100 h-fit px-2 rounded-md ml-2 font-thin border border-gray-200 hidden md:inline-block">
+                Software Engineer
+              </span>
+            </h2>
+            <h3 className="text-lg md:text-2xl">
+              Full Stack Web Developer with 2+ years of experience.
+            </h3>
+            <p className="text-lg md:text-2xl">
+              I <strong>design</strong>, <strong>code</strong>, and{" "}
+              <strong>deploy</strong> things that live on the web.
+            </p>
           </div>
           {/* “I’m Interested In” */}
           <div className="mt-16">
             <h2 className="text-2xl font-medium">I’m Interested In</h2>
             <div className="space-y-2 mt-3">
-                <p className="text-lg">* Solving complex problems with simple solutions</p>
-                <p className="text-lg">* Building products that truly help people</p>
-                <p className="text-lg">* Learning new technologies and improving every day</p>
-                <p className="text-lg">* Writing code that’s clean, clear, and scalable</p>
+              <p className="text-lg">
+                * Solving complex problems with simple solutions
+              </p>
+              <p className="text-lg">
+                * Building products that truly help people
+              </p>
+              <p className="text-lg">
+                * Learning new technologies and improving every day
+              </p>
+              <p className="text-lg">
+                * Writing code that’s clean, clear, and scalable
+              </p>
             </div>
           </div>
-
         </div>
       </div>
     </main>
