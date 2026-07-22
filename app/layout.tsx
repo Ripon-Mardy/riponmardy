b1f941dff3f030a48ec4c9ba483df401bf1c878e
Ripon-Mardy <mardyripon10@gmail.com>
2026-07-22 14:21:24 +0600

    fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,20 +43,25 @@ export default function RootLayout({
         />
 
         {/* google analytics  */}
+        {/* <!-- Google tag (gtag.js) --> */}
         <Script
+          async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
           strategy="afterInteractive"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-  `}
-        </Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+    `,
+          }}
+        />
 
         {/* vercel analytics  */}
         <Analytics />
